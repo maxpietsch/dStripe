ad1f06785cf2ab55bce9e019d3b78875654d5f56
Max Pietsch <mail@maxpietsch.com>
2021-10-04 12:26:34 +0100

    use root in dockerhub image

--- docker/make_dockerhub.sh
+++ docker/make_dockerhub.sh
@@ -6,16 +6,9 @@ set -x
 IMAGE_NAME=maxpietsch/dstripe
 IMAGE_TAG=0.1
 
-[ -z "$UID" ] && UID=$(id -u)
-[ -z "$GID" ] && GID=$(id -g)
-OSTYPE=$(uname)
-
 cd ../
 docker image build \
   --build-arg username=$USER \
-  --build-arg uid=$UID \
-  --build-arg gid=$GID \
-  --build-arg ostype=$OSTYPE \
   --file docker/Dockerfile \
   --tag $IMAGE_NAME:$IMAGE_TAG \
   .
